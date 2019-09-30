0441ef0d575878f55684a93c8b7df41ceca03e2d
Michael Grandison <african.genetic.survival@gmail.com>
2019-09-30 12:17:51 +0100

    solved object passing: user graphics,subtypes,user ref etc, user, content resovelrs compltet, group reslvr start

--- black_star_mind/graphql/resolvers/content.js
+++ black_star_mind/graphql/resolvers/content.js
@@ -1,46 +1,192 @@
-const Event = require('../../models/event');
-const User = require('../../models/user');
+const bcrypt = require('bcryptjs');
+const jwt = require('jsonwebtoken');
+const DataLoader = require('dataloader');
 
-const { transformEvent } = require('./merge');
+const User = require('../../models/user');
+const Group = require('../../models/group');
+const Perk = require('../../models/perk');
+const Content = require('../../models/content');
+const Action = require('../../models/action');
+const Interaction = require('../../models/interaction');
+const Search = require('../../models/search');
+
+const { transformContent } = require('./merge');
+const { dateToString } = require('../../helpers/date');
 
 module.exports = {
-  events: async () => {
+  contents: async (req) => {
+    // if (!req.isAuth) {
+    //   throw new Error('Unauthenticated!');
+    // }
     try {
-      const events = await Event.find();
-      return events.map(event => {
-        return transformEvent(event);
+      const contents = await Content.find();
+      return contents.map(content => {
+        return transformContent(content);
       });
     } catch (err) {
       throw err;
     }
   },
-  createEvent: async (args, req) => {
-    if (!req.isAuth) {
-      throw new Error('Unauthenticated!');
-    }
-    const event = new Event({
-      title: args.eventInput.title,
-      description: args.eventInput.description,
-      price: +args.eventInput.price,
-      date: new Date(args.eventInput.date),
-      creator: req.userId
-    });
-    let createdEvent;
+  getContentId: async (args, req) => {
+    // if (!req.isAuth) {
+    //   throw new Error('Unauthenticated!');
+    // }
+    // console.log(args.username);
     try {
-      const result = await event.save();
-      createdEvent = transformEvent(result);
-      const creator = await User.findById(req.userId);
-
-      if (!creator) {
-        throw new Error('User not found.');
-      }
-      creator.createdEvents.push(event);
-      await creator.save();
-
-      return createdEvent;
+      // const user = await User.findOne(args.username);
+      const content = await Content.findById(args._id);
+        return {
+            ...content._doc,
+            _id: content.id,
+            createdAt: content.createdAt,
+            updatedAt: content.updatedAt,
+            title: content.title,
+            domain: content.domain,
+            category: content.category,
+            creator: content.creator
+        };
     } catch (err) {
-      console.log(err);
       throw err;
     }
-  }
+  },
+  getContentTitle: async (args, req) => {
+    // if (!req.isAuth) {
+    //   throw new Error('Unauthenticated!');
+    // }
+    try {
+      const content = await Content.findOne({title: args.title});
+        return {
+            ...content._doc,
+            _id: content.id,
+            createdAt: content.createdAt,
+            updatedAt: content.updatedAt,
+            title: content.title,
+            domain: content.domain,
+            category: content.category,
+            creator: content.creator
+        };
+    } catch (err) {
+      throw err;
+    }
+  },
+  getContentDomain: async (args, req) => {
+    // if (!req.isAuth) {
+    //   throw new Error('Unauthenticated!');
+    // }
+    try {
+      const content = await Content.findOne({domain: args.domain});
+        return {
+            ...content._doc,
+            _id: content.id,
+            title: content.title,
+            domain: content.domain,
+            category: content.category,
+            creator: content.creator
+        };
+    } catch (err) {
+      throw err;
+    }
+  },
+  updateContent: async (args, req) => {
+    // if (!req.isAuth) {
+    //   throw new Error('Unauthenticated!');
+    // }
+    console.log(JSON.stringify(args));
+    try {
+      const content = await Content.findOneAndUpdate({_id:args.contentId},{
+        // {
+        title: args.contentInput.title,
+        domain: args.contentInput.domain,
+        category: args.contentInput.category,
+        creator: args.contentInput.creator
+      // }
+      },{new: true});
+        return {
+          ...content._doc,
+          _id: content.id,
+          title: content.title,
+          domain: content.domain,
+          category: content.category,
+          creator: content.creator
+        };
+    } catch (err) {
+      throw err;
+    }
+  },
+  updateContentData: async (args, req) => {
+    // if (!req.isAuth) {
+    //   throw new Error('Unauthenticated!');
+    // }
+    console.log(JSON.stringify(args));
+    try {
+      const content = await Content.findOneAndUpdate({_id:args.contentId},{$push: {data:args.contentDataInput}},{new: true});
+      // const user = await User.findById(userId);
+        return {
+          ...content._doc,
+          _id: content.contentId,
+          title: content.title,
+          domain: content.domain,
+          category: content.category,
+          creator: content.creator
+        };
+    } catch (err) {
+      throw err;
+    }
+  },
+  deleteContent: async (args, req) => {
+    // if (!req.isAuth) {
+    //   throw new Error('Unauthenticated!');
+    // }
+    // console.log(JSON.stringify(args));
+    try {
+      const content = await Content.findByIdAndRemove(args._id);
+      // const user = await User.findById(userId);
+        return {
+            ...content._doc,
+            _id: content.id,
+            createdAt: content.createdAt,
+            updatedAt: content.updatedAt,
+            title: content.title,
+            domain: content.domain,
+            category: content.category,
+            creator: content.creator
+        };
+    } catch (err) {
+      throw err;
+    }
+  },
+  createContent: async args => {
+    // if (!req.isAuth) {
+    //   throw new Error('Unauthenticated!');
+    // }
+    // console.log(JSON.stringify(args));
+    try {
+      const existingContent = await Content.findOne({ title: args.contentInput.title });
+      if (existingContent) {
+        throw new Error('Title exists already.');
+      }
+
+      const content = new Content({
+        title: args.contentDataInput.title,
+        domain: args.contentDataInput.domain,
+        category: args.contentDataInput.category,
+        creator: args.contentDataInput.creator
+      });
+
+      const result = await content.save();
+
+      return {
+        ...result._doc,
+        _id: result.id,
+        createdAt: result.createdAt,
+        updatedAt: result.updatedAt,
+        title: result.title,
+        domain: result.domain,
+        category: result.category,
+        creator: result.creator
+      };
+    } catch (err) {
+      throw err;
+    }
+  },
 };
--- black_star_mind/graphql/resolvers/user.js
+++ black_star_mind/graphql/resolvers/user.js
@@ -14,7 +14,10 @@ const { transformUser } = require('./merge');
 const { dateToString } = require('../../helpers/date');
 
 module.exports = {
-  users: async () => {
+  users: async (req) => {
+    // if (!req.isAuth) {
+    //   throw new Error('Unauthenticated!');
+    // }
     try {
       const users = await User.find();
       return users.map(user => {
@@ -24,16 +27,126 @@ module.exports = {
       throw err;
     }
   },
-  getUser: async (username) => {
-    console.log("here  "+username.username);
+  getThisUser: async (req) => {
+    // if (!req.isAuth) {
+    //   throw new Error('Unauthenticated!');
+    // }
+    // console.log(args.username);
     try {
-      const user = await User.findOne(username);
+      // const user = await User.findOne(args.username);
+      const user = await User.findById(req.userId);
+        return {
+            ...user._doc,
+            _id: user.id,
+            email: user.email,
+            password: user.password,
+            name: user.name,
+            username: user.username,
+            phone: user.phone,
+            address: user.address,
+            socialMedia: args.user.socialMedia
+        };
+    } catch (err) {
+      throw err;
+    }
+  },
+  getUserId: async (args, req) => {
+    // if (!req.isAuth) {
+    //   throw new Error('Unauthenticated!');
+    // }
+    // console.log(args.username);
+    try {
+      // const user = await User.findOne(args.username);
+      const user = await User.findById(args.userId);
+        return {
+            ...user._doc,
+            _id: user.id,
+            name: user.name,
+            username: user.username
+        };
+    } catch (err) {
+      throw err;
+    }
+  },
+  getUserUsername: async (args, req) => {
+    // if (!req.isAuth) {
+    //   throw new Error('Unauthenticated!');
+    // }
+    // console.log(args.username);
+    try {
+      const user = await User.findOne({username: args.username});
       // const user = await User.findById(userId);
         return {
             ...user._doc,
             _id: user.id,
             name: user.name,
             username: user.username
+        };
+    } catch (err) {
+      throw err;
+    }
+  },
+  updateUser: async (args, req) => {
+    // if (!req.isAuth) {
+    //   throw new Error('Unauthenticated!');
+    // }
+    console.log(JSON.stringify(args));
+    try {
+      const user = await User.findOneAndUpdate({_id:args.userId},{
+        // {
+        email: args.email,
+        password: args.userInput.password,
+        name: args.userInput.name,
+        username: args.userInput.username,
+        phone: args.userInput.phone,
+        address: args.userInput.address
+      // }
+      },{new: true});
+        return {
+            ...user._doc,
+            _id: user.id,
+            name: user.name,
+            username: user.username,
+            phone: user.phone,
+            demographics: user.demographics
+
+        };
+    } catch (err) {
+      throw err;
+    }
+  },
+  updateUserGraphics: async (args, req) => {
+    // if (!req.isAuth) {
+    //   throw new Error('Unauthenticated!');
+    // }
+    console.log(JSON.stringify(args));
+    try {
+      const user = await User.findOneAndUpdate({_id:args.userId},{$push: {demographics:args.userGraphicsInput}},{new: true});
+      // const user = await User.findById(userId);
+        return {
+            ...user._doc,
+            _id: user.id,
+            name: user.name,
+            username: user.username,
+            phone: user.phone,
+            demographics: user.demographics
+
+        };
+    } catch (err) {
+      throw err;
+    }
+  },
+  deleteUser: async (args, req) => {
+    // if (!req.isAuth) {
+    //   throw new Error('Unauthenticated!');
+    // }
+    // console.log(JSON.stringify(args));
+    try {
+      const user = await User.findByIdAndRemove(args.userId);
+      // const user = await User.findById(userId);
+        return {
+            ...user._doc,
+            _id: user.id
         };
     } catch (err) {
       throw err;
@@ -69,7 +182,17 @@ module.exports = {
 
       const result = await user.save();
 
-      return { ...result._doc, password: hashedPassword, _id: result.id };
+      return {
+        ...result._doc,
+        password: hashedPassword,
+        _id: result.id,
+        email: result.email,
+        name: result.name,
+        username: result.username,
+        phone: result.phone,
+        address: result.address,
+        socialMedia: result.socialMedia
+      };
     } catch (err) {
       throw err;
     }
