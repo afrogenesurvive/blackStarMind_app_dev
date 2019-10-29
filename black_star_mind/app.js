dcf86314cc161e32dcb462779a1e4adaef16b1af
Michael Grandison <african.genetic.survival@gmail.com>
2019-10-29 18:15:43 +0000

    fixed package json

--- black_star_mind/app.js
+++ black_star_mind/app.js
@@ -58,7 +58,7 @@ app.use(
   })
 );
 
-mongoose.connect(process.env.MONGO_URI,{useNewUrlParser: true})
+mongoose.connect(process.env.MONGO_URI,{useNewUrlParser: true, useUnifiedTopology: true})
   .then(() => {
     app.listen(5000);
   })
