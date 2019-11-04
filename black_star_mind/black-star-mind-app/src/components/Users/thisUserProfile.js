f15d65fe4956e62234247c56e1336e37eece8056
Michael Grandison <african.genetic.survival@gmail.com>
2019-11-04 14:26:08 +0000

    fixed user profile user object props passing!!! set thisUser context. were on our way

--- black_star_mind/black-star-mind-app/src/components/Users/thisUserProfile.js
+++ black_star_mind/black-star-mind-app/src/components/Users/thisUserProfile.js
@@ -1,36 +1,26 @@
 import React from 'react';
+import Card from 'react-bootstrap/Card';
+import Button from 'react-bootstrap/Button';
 
 import './thisUserProfile.css';
 
-const thisUserProfile = props => (
-    <div>
-    <p>Your Profile</p>
-      <h1>{props.authUserId}</h1>
-      <p>
-        ${props.userId}
-      </p>
-      <p>
-        ${props.name}
-      </p>
-      <p>
-        ${props.username}
-      </p>
-      <p>
-        ${props.email}
-      </p>
-      <p>
-        ${props.dob}
-      </p>
-      <p>
-        ${props.address}
-      </p>
-      <p>
-        ${props.phone}
-      </p>
-      <p>
-        ${props.socialMedia}
-      </p>
-    </div>
-);
+const thisUserProfile = (props) => {
+  const {...user} = props.user;
+  console.log("should be user object name..." + user.name);
+  console.log("should be user object id..." + user._id);
+  return (
+    <Card style={{ width: '18rem' }}>
+    <Card.Body>
+      <Card.Title>{props.authUserId}</Card.Title>
+      <h2>{user.name}</h2>}
+      <Card.Text>
+        Some quick example text to build on the card title and make up the bulk of
+        the card's content.
+      </Card.Text>
+      <Button variant="primary">Go somewhere</Button>
+    </Card.Body>
+  </Card>
+  );
+}
 
 export default thisUserProfile;
