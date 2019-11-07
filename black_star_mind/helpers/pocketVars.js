47add8a64e98eacd268fed19849e4fa4e134bede
Michael Grandison <african.genetic.survival@gmail.com>
2019-11-07 17:50:22 +0000

    fixed and tested is to poecket vars due to req object subscription conflict, token decoding in resolver

--- black_star_mind/helpers/pocketVars.js
+++ black_star_mind/helpers/pocketVars.js
@@ -1,27 +1,6 @@
 const pocketVariables = {
-    user: {
-      token: null,
-      _id: null,
-      email: null,
-      name: null,
-      username: null
-    },
-    thisUser: {
-      _id: null,
-      email: null,
-      name: null,
-      username: null
-    },
-    content: {
-      _id: null,
-      title: null,
-      creator: null
-    },
-    action: {
-      _id: null,
-      type: null,
-      targetId: null,
-      targetTitle: null
-    }
+    token: null,
+    userId: null,
+    isAuth: null
 }
 exports.pocketVariables = pocketVariables;
--- black_star_mind/middleware/is-auth.js
+++ black_star_mind/middleware/is-auth.js
@@ -9,28 +9,40 @@ const { pocketVariables } = require('../helpers/pocketVars');
 module.exports = (req, res, next) => {
   const authHeader = req.get('Authorization');
   if (!authHeader) {
-    req.isAuth = false;
+
+    pocketVariables.isAuth = false;
     return next();
   }
   const token = authHeader.split(' ')[1];
+  pocketVariables.token = token;
   if (!token || token === '') {
     req.isAuth = false;
+    pocketVariables.isAuth = false;
+
     return next();
   }
 
-  let decodedToken;
-  try {
-    decodedToken = jwt.verify(token, '5CleanStream');
-  } catch (err) {
-    req.isAuth = false;
-    return next();
-  }
-  if (!decodedToken) {
-    req.isAuth = false;
-    return next();
-  }
-  req.isAuth = true;
-  req.userId = decodedToken.userId;
+  // let decodedToken;
+  // try {
+  //   decodedToken = jwt.verify(token, '5CleanStream');
+  //   console.log("is-auth 3 pocketVars..." + JSON.stringify(pocketVariables), "decodedToken..." + decodedToken);
+  // } catch (err) {
+  //   req.isAuth = false;
+  //   pocketVariables.isAuth = false;
+  //   console.log("is-auth 4 pocketVars..." + JSON.stringify(pocketVariables), "decodedToken..." + decodedToken);
+  //   return next();
+  // }
+  // if (!decodedToken) {
+  //   req.isAuth = false;
+  //   pocketVariables.isAuth = false;
+  //   console.log("is-auth 5 pocketVars..." + JSON.stringify(pocketVariables));
+  //   return next();
+  // }
+  // req.isAuth = true;
+  // pocketVariables.isAuth = true;
+  // pocketVariables.userId = decodedToken.userId;
+  // req.userId = decodedToken.userId;
+  // console.log("is-auth 6 pocketVars..." + JSON.stringify(pocketVariables));
 
   // if (req.session.userId === req.userId) {
   //   console.log("login token/request token match");
